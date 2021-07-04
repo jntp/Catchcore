36403d33df9b9de4434e3f7dabc688e8d1df9eec
Justin Tang <justin.jntprules@gmail.com>
2021-07-04 00:05:48 -0700

    added a map to overlay watershed

--- main.py
+++ main.py
@@ -1,7 +1,30 @@
 import geopandas
+import matplotlib.pyplot as plt
+import cartopy.crs as ccrs
+import cartopy.feature as cfeature 
+
+# Create Map
+def new_map(fig, lon, lat):
+  # Create projection centered on the radar. Allows us to use x and y relative to the radar
+  proj = ccrs.LambertConformal(central_longitude = lon, central_latitude = lat)
+
+  # New axes with the specified projection
+  ax = fig.add_axes([0.02, 0.02, 0.96, 0.96], projection = proj)
+
+  # Add coastlines and states
+  ax.add_feature(cfeature.COASTLINE.with_scale('50m'), linewidth = 2)
+  ax.add_feature(cfeature.STATES.with_scale('50m'))
+
+  return ax
 
 gdf = geopandas.read_file("santa_ana_r_a.geojson")
-gdf
+gdf.to_crs(epsg = 102009) # North America Lambert Conformal Conic
+
+gdf["area"] = gdf.area
+
+gdf.plot("area", legend = True)
+plt.show() 
 
 # Test phase
 # You left off at meausring distance/making maps of geopandas introduction
+# Now overlay watershed onto world map... may need to change projection? 
