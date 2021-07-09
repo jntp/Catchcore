9e349d7154a3778661c9f13e99a4df63786a1417
Justin Tang <justin.jntprules@gmail.com>
2021-07-09 00:47:16 -0700

    used geopandas and cartopy to plot SoCal map with watershed boundaries

--- main.py
+++ main.py
@@ -4,9 +4,18 @@ import cartopy.crs as ccrs
 import cartopy.feature as cfeature 
 
 # Create Map
-def new_map(fig, lon, lat):
+def new_map(fig, lon, lat, gdf):
   # Create projection centered on the radar. Allows us to use x and y relative to the radar
   proj = ccrs.LambertConformal(central_longitude = lon, central_latitude = lat)
+  
+  # Convert into a 'proj4' string/dict compatible with GeoPandas
+  crs_proj4 = proj.proj4_init
+  df_ae = gdf.to_crs(crs_proj4) 
+
+  # Add new geometries here
+  # new_geometries = [proj.project_geometry(ii, src_crs = crs) for ii in df_ae['geometry'].values]
+
+  # fig, ax = plt.subplots(subplot_kw = {'projection': proj}
 
   # New axes with the specified projection
   ax = fig.add_axes([0.02, 0.02, 0.96, 0.96], projection = proj)
@@ -15,16 +24,37 @@ def new_map(fig, lon, lat):
   ax.add_feature(cfeature.COASTLINE.with_scale('50m'), linewidth = 2)
   ax.add_feature(cfeature.STATES.with_scale('50m'))
 
+  ax.add_geometries(df_ae['geometry'], crs = proj)
+
   return ax
 
-gdf = geopandas.read_file("santa_ana_r_a.geojson")
-gdf.to_crs(epsg = 102009) # North America Lambert Conformal Conic
+def main():
+  # Load watershed
+  watershed = geopandas.read_file("santa_ana_r_a.geojson")
 
-gdf["area"] = gdf.area
+  # Create new map based off of CRS Projection
+  proj = ccrs.LambertConformal(central_longitude = -117.636, central_latitude = 33.818)
 
-gdf.plot("area", legend = True)
-plt.show() 
+  # Create a new figure
+  fig = plt.figure(figsize = (10, 10))
 
-# Test phase
-# You left off at meausring distance/making maps of geopandas introduction
-# Now overlay watershed onto world map... may need to change projection? 
+  # New axes with the specified projection
+  ax = fig.add_axes([0.02, 0.02, 0.96, 0.96], projection = proj)
+
+  # Set limits in lat/lon space
+  ax.set_extent([-121, -114, 32, 36])
+
+  # Add coastlines andd states
+  ax.add_feature(cfeature.COASTLINE.with_scale('50m'), linewidth = 2)
+  ax.add_feature(cfeature.STATES.with_scale('50m'))
+
+  # Add watershed geometry
+  ax.add_geometries(watershed.geometry, crs = ccrs.PlateCarree(), zorder = 5, edgecolor = 'blue')
+
+  plt.show()
+
+if __name__ == '__main__':
+  main()
+
+# Next step... Clean up code
+# Also next big step would be figuring out to incorporate NCFR core as "polygons"
