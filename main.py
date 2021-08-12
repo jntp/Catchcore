b0aa357eb443ce3002b882cdf99d9dc39290777a
Justin Tang <justin.jntprules@gmail.com>
2021-08-11 18:39:36 -0700

    finished optimizing reflectivity matrix code... now onto identifying ncfrs

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import cartopy.feature as cfeature
 from netCDF4 import Dataset
 import numpy as np
 import metpy.plots as mpplots 
+from shapely.geometry import Polygon
 
 # Create a base map to display Watershed and 
 def new_map(fig, lon, lat):
@@ -69,29 +70,33 @@ def main():
   j = indices[1]
   results = refs[refs >= 5]
 
-  # To find coordinate, take i * 1506 + j 
+  # Create new matrix of indices where reflectivity >= 5 that is retrofitted for numpy.put
+  full_indices = i * 1506 + j 
 
-  # How to make an array of the results via incremental i and j (no for loops please...)?
-
-  testmatrix.put([testindex1, testindex2], [24, -56])
-
-  # Find a more efficient way for this? this is too slow***
-  # for i, column in enumerate(refs):
-    # for j, value in enumerate(column):
-      # if refs[i][j] >= 5:
-        # test[i][j] = refs[i][j]
+  # Take the reflectivity values that meet the standard and put in new_refs matrix
+  np.put(new_refs, full_indices, results)
 
   # Add watershed geometry 
   ax.add_geometries(watershed.geometry, crs = ccrs.PlateCarree(), zorder = 1, facecolor = 'red', edgecolor = 'red')
 
+  # Identify NCFR; use reflectivity >= 45 dbz as the ncfr threshold
+  ncfr_indices = np.where(new_refs >= 45)
+  m = ncfr_indices[0]
+  n = ncfr_indices[1]
+  ncfr_refs = new_refs[new_refs >= 45] 
+  
+  # Create NCFR polygon
+  # Can create matrix full of zeros with length m and n
+  # Numpy should have a function stringing together x and y variables? 
+  # Plug into geopandas polygon function  
+
   # Add colormesh (radar reflectivity) 
-  ax.pcolormesh(x, y, test, cmap = ref_cmap, norm = ref_norm, zorder = 2) 
+  ax.pcolormesh(x, y, new_refs, cmap = ref_cmap, norm = ref_norm, zorder = 2) 
  
-  # plt.show()
+  plt.show()
 
 if __name__ == '__main__':
   main() 
 
-# Address Github Deprecation Notice!!!
-# First clean, optimize, and annotate code
 # Next step identify NCFR and the intersection
+# Clean code later
